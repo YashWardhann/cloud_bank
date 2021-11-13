f5fc35f394da99cbde597e927febe149b1f80ec0
Raunak Chopra <raunakchopra261@gmail.com>
2021-11-14 04:30:54 +0800

    Added Dropdown User menu to dashboard

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -2,3 +2,11 @@ $(".item").click(e => {
     $(".selected").removeClass("selected");
     $(e.target).addClass("selected");
 });
+
+let displayMenu = false;
+
+const toggleUserMenu = () => {
+    let element = document.getElementById('user-menu')
+    displayMenu = !displayMenu;
+    (displayMenu) ? element.style.display = "none" : element.style.display = "block";
+}

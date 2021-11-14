644d6db3fb71d1dc58fc60e50ef6b0cb77f43a97
yash <yash20008@gmail.com>
2021-11-14 21:52:43 +0800

    Added Changing Functionality

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,4 +1,6 @@
 $(".item").click(e => {
     $(".selected").removeClass("selected");
     $(e.target).addClass("selected");
+    const text = $(e.target).children('span');
+    $('.content-heading').text(text.text());
 });
